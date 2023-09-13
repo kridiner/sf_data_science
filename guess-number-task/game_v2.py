631a709aeba2efef3df64d1f0ced24c64413eb3c
kridiner <kridiner@mail.ru>
2023-09-13 16:47:06 +0300

    game_core-V3

--- guess-number-task/game_v2.py
+++ guess-number-task/game_v2.py
@@ -68,7 +68,38 @@ def game_core_v2(number: int = 1) -> int:
     return count
 
 
+def game_core_v3(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число, а потом уменьшаем
+    или увеличиваем его методом "деления отрезка пополам" в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток
+       
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict = np.random.randint(1, 101)
+    
+    while number != predict:
+        count += 1
+        if number > predict:
+            tmp = (number - predict) // 2
+            if tmp == 0:
+                tmp = 1
+            predict += tmp
+        elif number < predict:
+            tmp = (predict - number) // 2
+            if tmp == 0:
+                tmp = 1
+            predict -=  tmp
+
+    return count
+
+
 if __name__ == "__main__":
     # RUN
     score_game(random_predict)
     score_game(game_core_v2)
+    score_game(game_core_v3)
